5565ca8eef771c2581c6b90c9649f2919a3bcbe3
Aimal Amiri <meaimal@hotmail.com>
2022-05-26 04:13:54 +0430

    Add keys to google passport strategy

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port http://localhost:${PORT}`)
+  console.log(`Example app listening on port http://localhost:${PORT}`);
 });
